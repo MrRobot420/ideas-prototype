dfd4cc0face59d651d0afa6f493d7ce62e293532
MrRobot420 <max.karlchen@web.de>
2021-06-03 22:50:40 +0200

    add random faces and random genders to profile pic | add icons to show the tags

--- src/components/SideDrawer.vue
+++ src/components/SideDrawer.vue
@@ -9,7 +9,7 @@
     >
       <v-list-item>
         <v-list-item-avatar>
-          <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
+          <v-img :src="imageUrl"></v-img>
         </v-list-item-avatar>
 
         <v-list-item-content>
@@ -25,13 +25,13 @@
           :key="item.tag"
           link
         >
-          <v-list-item-icon>
-            <v-icon>mdi-{{ item.tag }}</v-icon>
-          </v-list-item-icon>
 
           <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
+            <v-list-item-title>{{ item.tag }}</v-list-item-title>
           </v-list-item-content>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
@@ -46,22 +46,32 @@ export default {
         drawerIsVisible: true,
         drawerSetPoint: true,
         items: [
-            { title: "Create Website", tag: "coding", effort: "medium", id: "ansiASh9823" },
-            { title: "Sell old stuff", tag: "selloff", effort: "easy", id: "OAhd98ABaas" },
-            { title: "Build businessmodel", tag: "enterprise", effort: "high", id: "PAd7asb38as" }
-        ]
+            { title: "Create Website", tag: "coding", effort: "medium", id: "ansiASh9823", icon: "mdi-code-braces" },
+            { title: "Sell old stuff", tag: "selloff", effort: "easy", id: "OAhd98ABaas", icon: "mdi-cash" },
+            { title: "Build businessmodel", tag: "enterprise", effort: "high", id: "PAd7asb38as", icon: "mdi-domain" }
+        ],
+        baseUrl: 'https://randomuser.me/api/portraits/',
+        gender: '',
+        dataFormat: '.jpg',
+        imageUrl: ''
     }),
     created() {
-        // console.log(this.drawerIsVisible)
         this.drawerIsVisible = this.isVisible
         this.drawerSetPoint = this.isVisible
     },
     watch: {
         isVisible(val) {
-            console.log(val);
+            if (val === true) {
+                this.randomBinary = Math.round(Math.random())
+                if (this.randomBinary === 1) {
+                    this.gender = 'men'
+                } else {
+                    this.gender = 'women'
+                }
+                this.imageUrl = `${this.baseUrl}${this.gender}/${Math.round(Math.random() * 100)}${this.dataFormat}`
+            }
             this.drawerIsVisible = val
             this.drawerSetPoint = val
-            console.log('TEST: ', this.drawerIsVisible)
         }
     }
 }

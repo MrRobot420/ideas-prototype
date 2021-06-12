3e1dc57c78c6fecca513f217c4c3274c6239e628
MrRobot420 <max.karlchen@web.de>
2021-06-12 22:00:42 +0200

    improve click-outside directive with vue-clickaway library

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <v-app>
     <v-main>
       <AppBar @drawerClicked="toggleDrawer()"/><br>
-      <SideDrawer ref="sideDrawer" :isVisible="drawerVisible" v-click-outside="hideDrawer"/>
+      <SideDrawer ref="sideDrawer" :isVisible="drawerVisible" v-on-clickaway="hideDrawer"/>
       <router-view/>
     </v-main>
   </v-app>
@@ -11,26 +11,13 @@
 <script>
 import AppBar from './components/AppBar'
 import SideDrawer from './components/SideDrawer'
-
-import Vue from 'vue'
-Vue.directive('click-outside', {
-  bind: function (el, binding, vnode) {
-    el.clickOutsideEvent = function (event) {
-      // here I check that click was outside the el and his children
-      if (!(el == event.target || el.contains(event.target))) {
-        // and if it did, call method provided in attribute value
-        vnode.context[binding.expression](event);
-      }
-    };
-    document.body.addEventListener('click', el.clickOutsideEvent)
-  },
-  unbind: function (el) {
-    document.body.removeEventListener('click', el.clickOutsideEvent)
-  },
-});
+import { directive as onClickaway } from 'vue-clickaway';
 
 export default {
   name: 'App',
+  directives: {
+    onClickaway: onClickaway,
+  },
   components: {
     AppBar,
     SideDrawer
